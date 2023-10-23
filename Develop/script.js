3c32b552f24e0c86d9af5b71cc7f6e37c917181e
Flesh <olivia.a.gowan@gmail.com>
2023-10-23 18:35:22 -0400

    wrote the randomization

--- Develop/script.js
+++ Develop/script.js
@@ -5,26 +5,28 @@ var passwordLength = null;
 var wantsNumbers = null;
 var wantsSpecial = null;
 var passwordCharacters = [];
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // should get back an array with 26 characters.
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // Should get back an array with 26 characters.
 var lowercaseLetters = "abcedfghijklmnopqrstuvwxyz".split("");
 var numbers = "1234567890".split("");
 var specialCharacters = `!"#$%&'()*+,-./:;<=>?@[\\]^_{|}~`.split("");
+var randomCharacters = null;
+var passwordResult = null;
 
 function generatePassword() {
-  passwordLength = window.prompt("Password Length - Between 8 and 128 characters");
-  // the user types in a silly response, like the word "jam"
-  // check if passwordLength is a number
-  // how do I do that?
+  passwordLength = window.prompt("Choose a password Length - Between 8 and 128 characters");
+  
+  // Validate that the user enters a number within the range
   passwordLength = parseInt(passwordLength);
   if (isNaN(passwordLength)) {
     window.alert("Please enter an appropriate number.");
     return null;
   }
   if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Sorry, too short or too long.");
+    window.alert("Sorry, that is not within the given range.");
     return null;
   };
-  
+
+  // Ask the user which characters they want in their password
   wantsUppercase = window.confirm("Do you want uppercase characters in your pasword?");
   wantsLowercase = window.confirm("Do you want lower case characters in your password?");
   wantsNumbers = window.confirm("Do you want numbers in your password?"); 
@@ -42,11 +44,18 @@ function generatePassword() {
   if (wantsSpecial) {
     passwordCharacters.concat(specialCharacters);
   };
-  //validate that the user actually selects one of the options
+
+  // Validate that the user actually selects one of the options
   if (!(wantsUppercase || wantsLowercase || wantsNumbers || wantsSpecial)) {
     window.alert("Sorry, you need to select at least one type of character to include in your password. Please try again.");
     return generatePassword();
   };
+
+  // Randomize password using the parameters taken from the user's responses
+  randomCharacters = Math.floor(Math.random() * passwordCharacters.length);
+  passwordResult = passwordCharacters[randomCharacters];
+    
+  return passwordResult;
 };
 
 
